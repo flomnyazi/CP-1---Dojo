74e8283c971ad0e49918decf0986ee3ac7b6bdf6
Mnyazi <mnyazi@192.168.1.2>
2017-06-15 19:35:38 +0300

    changes to root folder

---
+++ rooms/rooms.py
@@ -0,0 +1,18 @@
+class Room(object):
+    def_init_(self, name):
+        self.name = name
+
+
+class Office(Room):
+    capacity = 6
+    def_init_(self, name):
+        self.name = name
+
+
+class LivingSpace(Room):
+    capacity = 4
+    def_init_(self, name):
+        self.name = name
+
+
+    
